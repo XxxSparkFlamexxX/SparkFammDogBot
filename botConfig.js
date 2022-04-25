16ecf5576362d4e13bb2a6a0facc7fc26505e114
XxxSparkFlamexxX <101583007+XxxSparkFlamexxX@users.noreply.github.com>
2022-04-24 22:26:01 -0400

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "OTY3ODc5ODgyMDIzNTEwMDE2.YmWuYg.P04a1Pfnfaizbavu0ZCT_g3P7J4",
+    prefix: "!",
+    admins: [
+        "881839404300259349"
+],
+    debug: true,
+    countChannel: "931875095767556136"
+};
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Yo boi!!'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
+const Discord = require("discord.js");
+ const client = new Discord.Client({ disableMentions: 'everyone' });
+const Eco = require("quick.eco");
+client.eco = new Eco.Manager(); // quick.eco
+client.db = Eco.db; // quick.db
+client.config = require("./botConfig");
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.shop = {
+  DogHouse: {
+    cost: 50000
+  },
+  Ball: {
+    cost: 7000
+  },  
+  DinosaurBone: {
+    cost: 5000
+  },
+  Frisbee: {
+    cost: 7000
+  },
+  Sausage: {
+    cost: 5000
+  },
+  SqueakyToy: {
+    cost: 7000
+  }
+},
+const fs = require("fs");
+
+fs.readdir("./events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(f => {
+        if (!f.endsWith(".js")) return;
+        const event = require(`./events/${f}`);
+        let eventName = f.split(".")[0];
+        client.on(eventName, event.bind(null, client));
+    });
+});
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(f => {
+        if (!f.endsWith(".js")) return;
+        let command = require(`./commands/${f}`);
+        client.commands.set(command.help.name, command);
+        command.help.aliases.forEach(alias => {
+            client.aliases.set(alias, command.help.name);
+        });
+    });
+});
+
+
+client.login(client.config.token);
