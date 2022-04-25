e12e9d1290ee599a9dc1d81c0001b035f487539d
XxxSparkFlamexxX <101583007+XxxSparkFlamexxX@users.noreply.github.com>
2022-04-24 22:44:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000
-
-app.get('/', (req, res) => res.send('Yo boi!!'))
-
-app.listen(port, () =>
-console.log(`Your app is listening a http://localhost:${port}`)
-);
 const Discord = require("discord.js");
  const client = new Discord.Client({ disableMentions: 'everyone' });
 const Eco = require("quick.eco");
